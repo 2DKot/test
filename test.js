7f6a6b9fb1358a32f6add2e257246343cd544d0c
2DKot <baidinkn@ya.ru>
2016-08-14 17:37:20 +0300

    wrong test

--- test.js
+++ test.js
@@ -4,4 +4,8 @@ describe("pow", function() {
   it("возводит в n-ю степень", function() {
     assert.equal(Math.pow(2, 3), 8);
   });
+
+  it("возводит в n-ю степень", function() {
+    assert.equal(Math.pow(2, 2), 5);
+  });
 });
